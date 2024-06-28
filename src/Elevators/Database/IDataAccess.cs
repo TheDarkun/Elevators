54794f115899a1d3003720c6de13b099c91c7da9
Darkun <106868917+TheDarkun@users.noreply.github.com>
2024-06-28 10:40:30 +0200

    Added database access with simple example; Updated gitignore

---
+++ src/Elevators/Database/IDataAccess.cs
@@ -0,0 +1,19 @@
+﻿using Elevators.Models;
+
+namespace Elevators.Database;
+
+public interface IDataAccess
+{
+    /// <summary>
+    /// Creates or updates a user model in the database asynchronously.
+    /// </summary>
+    /// <param name="userModel">The user model to create or update.</param>
+    public Task CreateOrUpdateUserModelAsync(UserModel userModel);
+    
+    /// <summary>
+    /// Retrieves a user model by its ID asynchronously.
+    /// </summary>
+    /// <param name="id">The ID of the user.</param>
+    /// <returns>The user model if found, or null.</returns>
+    public Task<UserModel?> GetUserModelByIdAsync(ulong id);
+}
---
+++ src/Elevators/Models/UserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Elevators.Models;
+
+public class UserModel(long id, long wins, long loses)
+{
+    public long Id { get; set; } = id;
+    public long Wins { get; set; } = wins;
+    public long Loses { get; set; } = loses;
+}
