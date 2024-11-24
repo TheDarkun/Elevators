fc78f5b6989c6a50b926dc15a484c74c42b36f4a
Darkun <106868917+TheDarkun@users.noreply.github.com>
2024-11-24 11:06:37 +0100

    Added migrations

---
+++ src/Elevators.Api/Migrations/20241124093211_AddedSelectedGuildDb.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Elevators.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSelectedGuildDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SelectedGuilds",
+                columns: table => new
+                {
+                    GuildId = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PlayedGames = table.Column<int>(type: "INTEGER", nullable: false),
+                    JoinedUsers = table.Column<string>(type: "TEXT", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    GameRoomId = table.Column<long>(type: "INTEGER", nullable: false),
+                    TopFloor = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SelectedGuilds", x => x.GuildId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SelectedGuilds");
+        }
+    }
+}
---
+++ src/Elevators.Api/Migrations/20241124095641_AddedIdToSelectedGuilds.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Elevators.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIdToSelectedGuilds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SelectedGuilds",
+                table: "SelectedGuilds");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "GuildId",
+                table: "SelectedGuilds",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "SelectedGuilds",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SelectedGuilds",
+                table: "SelectedGuilds",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SelectedGuilds",
+                table: "SelectedGuilds");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "SelectedGuilds");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "GuildId",
+                table: "SelectedGuilds",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SelectedGuilds",
+                table: "SelectedGuilds",
+                column: "GuildId");
+        }
+    }
+}
